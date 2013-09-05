cb0daf0c45b4da7b305281f41f84d2b57c31aad9
Matt Portune <mportune@imobile3.com>
2013-09-05 10:52:38 -0400

    Tweaked CensorHelper (excuse to test github push)

--- Fridgeface/src/com/fridgeface/utils/CensorHelper.java
+++ Fridgeface/src/com/fridgeface/utils/CensorHelper.java
@@ -2,6 +2,7 @@
 package com.fridgeface.utils;
 
 import java.util.HashMap;
+import java.util.Locale;
 
 public class CensorHelper {
     private HashMap<String, String> mReplacementMap;
@@ -10,12 +11,16 @@ public class CensorHelper {
         mReplacementMap = new HashMap<String, String>();
 
         // Doin' it Moronie style
+        mReplacementMap.put("shit", "shtie");
+        mReplacementMap.put("bastard", "bastage");
+        mReplacementMap.put("bitch", "batch");
         mReplacementMap.put("ball", "bell");
         mReplacementMap.put("ass", "ice");
         mReplacementMap.put("fuck", "farg");
     }
 
     public String cleanUp(String phrase) {
+        phrase = phrase.toLowerCase(Locale.US);
         for (String key : mReplacementMap.keySet()) {
             phrase = phrase.replaceAll(key, mReplacementMap.get(key));
         }
